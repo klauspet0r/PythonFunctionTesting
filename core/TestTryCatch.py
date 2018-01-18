04f7804d78f13cf598b660d8bc9b8a28f1bd5e5d
Michal Zajac <michal.zajac@ml-pa.com>
2018-01-18 14:17:44 +0100

    working

--- core/TestTryCatch.py
+++ core/TestTryCatch.py
@@ -1,10 +1,12 @@
 import sqlite3 
 import json
+from logzero import logger
+import sys
 
 
 
 
-json_string = '{"Test":{"Name":"getAvailableLogLevels","Description":"This test checks if the command getAvailableLogLevels works properly"},"TestDefinition":{"TestName":"getAvailableLogLevels","RobotCodeLocation":"./robot/getAvailableLogLevels.robot","Comment":"This is an comment about some robot stuff"},"Dependencies":{"Parameters":[{"domain":"messaging","name":"json_schema_command","version":0},{"domain":"messaging","name":"json_schema_response","version":1}]}}'
+json_string = '{"Test":{"Name":"getAvailableLogLevels","Description":"This test checks if the command getAvailableLogLevels works properly"},"TestDefinition":{"TestName":"getAvailableLogLevels","RobotCodeLocation":"./robot/getAvailableLogLevels.robot","Comment":"This is an comment about some robot stuff"},"Dependencies":{"Parameters":[{"domain":"messaging","name":"json_schema_command","version":0},{"domain":"mmessaging","name":"json_schema_response","version":1}]}}'
 
 json_object = json.loads(json_string)
 
@@ -17,10 +19,16 @@ conn = sqlite3.connect(db_adress)
 
 c = conn.cursor()
 
-for entry in json_object.get("Dependencies").get("Parameters"):
-    c.execute('''SELECT EXISTS(SELECT * FROM Parameter WHERE Domain = '{}' AND Name = '{}' AND Version = '{}')'''.format(entry.get("domain"), entry.get("name"), entry.get("version")))
-    q_result = c.fetchall()
-    if q_result[0][0] == 1:
-        print "Parameterset exsists in DB"
-    elif q_result[0][0] == 0:
-        print "Parameterset NOT in DB"
+try:
+    for entry in json_object.get("Dependencies").get("Parameters"):
+        c.execute('''SELECT EXISTS(SELECT * FROM Parameter WHERE Domain = '{}' AND Name = '{}' AND Version = '{}')'''.format(entry.get("domain"), entry.get("name"), entry.get("version")))
+        q_result = c.fetchall()
+        if q_result[0][0] == 0:
+            raise sqlite3.IntegrityError
+except sqlite3.IntegrityError:
+    logger.error("You are trying to add a test with a dependency that does not exist in the database")
+    sys.exit('')
+    
+logger.info("All good!")
+    
+                
