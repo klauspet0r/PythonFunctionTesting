568464d6266ea290278c8330f9e547fc12c8c79f
Michal Zajac <michal.zajac@ml-pa.com>
2018-01-19 15:17:31 +0100

    Testing RegEx

--- core/RegExFun.py
+++ core/RegExFun.py
@@ -6,9 +6,9 @@ with open('getCpuUsage.robot') as robot_file:
     
 # print robot_string
 
-match = re.compile('\*{3} Variables \*{3}')
+regex_pattern_object = re.compile('\*{3} Variables \*{3}')
 
-for x in match.findall(robot_string):
+for x in regex_pattern_object.findall(robot_string):
     print x
 
 print re.sub('\*{3} +Variables +\*{3}', '*** Variables ***\n${var1}  value1\n${var2}  value2 ', robot_string)
