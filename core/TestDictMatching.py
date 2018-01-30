1c6e7cfe00397c22ea9018c04c6cf9ae3fc2ea21
Michal Zajac <michal.zajac@ml-pa.com>
2018-01-30 14:46:20 +0100

    recursive way to check two dicts

---
+++ core/TestDictMatching.py
@@ -0,0 +1,23 @@
+import json
+
+payload = json.loads('{"Type":"RESPONSE","Name":"getSupportStatus","Data":{"ResponseValue":{"Message":"No Support Console is Running","TargetHost":"","TargetPort":0},"ResultType":"SUCCESS"},"Tag":"46528925-6021-3438-9773-997055251222","TimeStamp":1517239324927}')
+
+payload_match = json.loads('{"Data":{"ResponseValue":{"Message":"No Support Console is Running"}}}')
+
+
+def match_dicts(small, big):
+    print small, big
+    if type(big) != type(small):
+        return False
+    if type(small) != dict:
+        return (small == big)
+    for key in big.keys():        
+        if key == small.keys()[0]:
+            if match_dicts(small[key],big[key]):
+                return True
+            else:
+                continue
+    return False
+            
+            
+print match_dicts(payload_match, payload)                 
