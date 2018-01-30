cf678dc725fe84ecfc856a91d241aab09dd43d2f
Michal Zajac <michal.zajac@ml-pa.com>
2018-01-30 17:23:11 +0100

    encapsulated for use with strings insead of json-objects

--- core/TestDictMatching.py
+++ core/TestDictMatching.py
@@ -1,22 +1,29 @@
 import json
 
-payload = json.loads('{"Type":"RESPONSE","Name":"getSupportStatus","Data":{"ResponseValue":{"Message":"No Support Console is Running","TargetHost":"","TargetPort":0},"ResultType":"SUCCESS"},"Tag":"46528925-6021-3438-9773-997055251222","TimeStamp":1517239324927}')
 
-payload_match = json.loads('{"Data":{"ResponseValue":{"Message":"No Support Console is Running"}}}')
+payload = '{"Type":"RESPONSE","Name":"getSupportStatus","Data":{"ResponseValue":{"Message":"No Support Console is Running","TargetHost":"","TargetPort":0},"ResultType":"SUCCESS"},"Tag":"46528925-6021-3438-9773-997055251222","TimeStamp":1517239324927}'
+
+payload_match = '{"Data":{"ResponseValue":{"Message":"No Support Console is Running"}}}'
 
 
-def match_dicts(small, big):    
-    if type(big) != type(small):
-        return False
-    if type(small) != dict:
-        return (small == big)
-    for key in big.keys():        
-        if key == small.keys()[0]:
-            if match_dicts(small[key],big[key]):
-                return True
-            else:
-                continue
-    return False
+def check_content(small, big):
+        small_json = json.loads(small)
+        big_json = json.loads(big)        
+    
+        def match_dicts(small,big):    
+            if type(big) != type(small):
+                return False
+            if type(small) != dict:
+                return (small == big)
+            for key in big.keys():        
+                if key == small.keys()[0]:     
+                    if match_dicts(small[key],big[key]) or match_dicts(small,big[key]):
+                        return True
+                    else:
+                        continue
+            return False
+        
+        return match_dicts(small_json, big_json)
             
             
-print match_dicts(payload_match, payload)                 
+print check_content(payload_match, payload)                 
